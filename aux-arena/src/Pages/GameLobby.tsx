2533d49eca20efd00eb9ce96e2d425350a31c5c1
bleaky11 <levichen533@gmail.com>
2025-11-05 10:04:21 -0500

    Updated player type

--- aux-arena/src/Pages/GameLobby.tsx
+++ aux-arena/src/Pages/GameLobby.tsx
@@ -1,35 +1,62 @@
-import { useEffect, useState } from "react";
-//import { io } from "socket.io-client";
+import { useEffect, useMemo, useState } from "react";
 import "./Lobby.css"
 import { useLocation } from "react-router-dom";
-
-//const socket = io("http://localhost:3000"); //Placeholder, change this to server ip
 
 export interface information{
   user: string;
   loggedIn: string;
 }
 
+type UserSession = {
+    userID: number;
+    sessionID: number;
+    displayName: string;
+    lobbyID: number;
+    lastPingTime: string; //Java type instant, *should* be sent as an iso string
+    active: boolean;
+    isSpectator: boolean;
+}
+
 export default function GameLobby({user, loggedIn}: information) {
-    const [playerList, updatePlayerList] = useState<string[]>([user, "user2", "user3", "user3"])
-    const [playerCount, updateCount] = useState<number>(playerList.length)
     const [maxPlayer] = useState<number>(20) // Not letting change in lobby
     const location = useLocation()
     const {lobby} = location.state 
+    const [playerList, updatePlayerList] = useState<UserSession[]>([])
+    const playerCount = playerList.length
 
+    const player:UserSession = {userID: 1, sessionID: 1, displayName: user,
+         lobbyID:lobby, lastPingTime:"9:38AM", active:true, isSpectator:false}
+
+    function updatePlayers(newPlayer:UserSession){
+        if(playerCount < 20)
+            updatePlayerList([...playerList, newPlayer])
+        else
+            alert("Max players exceeded")
+    }
+    useEffect(()=>{
+        updatePlayers(player)
+    }, [])
+
+    function addPlayer(){ //For testing 
+        const newPlayer:UserSession = {userID: 1, sessionID: 1, displayName: "newUser",
+         lobbyID:lobby, lastPingTime:"9:38AM", active:true, isSpectator:false}
+         updatePlayers(newPlayer)
+    }
 
     /*useEffect(()=> {//Use to update playercount when new players join
-        function updatePlayers(newPlayers:string){
-            updatePlayerList([...playerList, ...newPlayers])
+        function updatePlayers(newPlayer:string){
+            updatePlayerList([...playerList, newPlayer])
             updateCount(playerList.length)
         }
 
-        socket.on("playerListUpdate", (data:any)=>{
-            updatePlayers(data.players);
+        socket.on("Update", (data:any)=>{
+            if(data.type == MessageEvent.USERJOINED){
+                updatePlayers(data.payload);
+            }   
         })
 
         return ()=>{
-            socket.off("playerListUpdate")
+            socket.off("updates")
         };
 
     }, [playerList])*/
@@ -41,11 +68,13 @@ export default function GameLobby({user, loggedIn}: information) {
                 <div>Lobby ID: {lobby}</div>
                 <div>{}</div>
             </div>
+
+            <button onClick={addPlayer} className="button">Debug</button>
                 
             <div className="playerbox">
                 {playerList.map((player, index)=>(
                     <div key={index} className="players">
-                        {player}
+                        {player.displayName} 
                     </div>
                 ))}
             </div>
