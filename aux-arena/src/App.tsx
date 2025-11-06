d4ae748ed6fee0043d433040e491e9671e12ffc7
Derek Johnson <howpopsir@gmail.com>
2025-11-05 21:35:37 -0500

    Very basic rxstomp connection to app

--- aux-arena/src/App.tsx
+++ aux-arena/src/App.tsx
@@ -1,8 +1,31 @@
+import { RxStomp } from '@stomp/rx-stomp';
+import { RxStompConfig } from '@stomp/rx-stomp';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const rxstomp = new RxStomp();
+const rxStompConfig: RxStompConfig = {
+  brokerURL: 'ws://localhost:3000/ws',
+  connectHeaders: {
+    username: 'guest',
+    userID: 'num',
+  },
+  debug: (msg) => {
+    console.log(new Date(), msg)
+  },
+  heartbeatIncoming: 0,
+  heartbeatOutgoing: 20000,
+  reconnectDelay: 200,
+}
+
 function App() {
+
+  rxstomp.configure(rxStompConfig)
+  rxstomp.activate()
+  rxstomp.deactivate()
+
   return (
     <div className="App">
       <header className="App-header">
